7798fc7c88b2e31f71fb3c0fa81d508ef6a140a2
jitrawan <jitrawan.ch@gmail.com>
2019-03-08 09:39:40 +0700

    commit

--- dashboard/members/member_history.php
+++ dashboard/members/member_history.php
@@ -4,7 +4,7 @@ $getmember_detail = $getdata->my_sql_query(NULL,"dealer","dealer_id='".addslashe
 <div class="row">
      <div class="col-lg-12">
              <h1 class="page-header"><i class="fa fa-list fa-fw"></i> <?php echo @LA_LB_DETAIL_PRODUCT;?></h1>
-     </div>        
+     </div>
 </div>
 <ol class="breadcrumb">
   <li><a href="index.php"><?php echo @LA_MN_HOME;?></a></li>
@@ -13,14 +13,14 @@ $getmember_detail = $getdata->my_sql_query(NULL,"dealer","dealer_id='".addslashe
 </ol>
 <div class="panel panel-primary">
                         <div class="panel-heading">
-                           <?php echo @LA_LB_SUPPLIER_DETAIL;?>                          
+                           <?php echo @LA_LB_SUPPLIER_DETAIL;?>
                         </div>
                         <div class="table-responsive">
                         <table width="100%" border="0" class="table">
   <tr>
     <td width="38%"><strong><?php echo @LA_LB_NAME_CHECKIN;?></strong></td>
     <td width="41%">&nbsp;<?php echo @$getmember_detail->dealer_name;?></td>
-   
+
   </tr>
   <tr>
     <td><strong><?php echo @LA_LB_NO;?></strong></td>
@@ -38,11 +38,11 @@ $getmember_detail = $getdata->my_sql_query(NULL,"dealer","dealer_id='".addslashe
     <td><strong><?php echo @LA_LB_EMAIL;?></strong></td>
     <td colspan="2">&nbsp;<?php echo @$getmember_detail->email;?></td>
   </tr>
- 
+
 </table>
 
                         </div>
-                       
+
 </div>
  <div class="panel panel-green">
                         <div class="panel-heading">
@@ -53,29 +53,38 @@ $getmember_detail = $getdata->my_sql_query(NULL,"dealer","dealer_id='".addslashe
                       <thead>
   <tr>
     <td width="3%" align="center" bgcolor="#CCCCCC"><strong>#</strong></td>
-    <td width="32%" align="center" bgcolor="#CCCCCC"><strong>รหัสสินค้า</strong></td>
-    <td width="21%" align="center" bgcolor="#CCCCCC"><strong>ชื่อสินค้า</td>
-    <td width="19%" align="center" bgcolor="#CCCCCC"><strong>ประเภท</td>
-    <td width="25%" align="center" bgcolor="#CCCCCC"><strong>ยี่ห้อ</td>
+    <td width="32%" align="center" bgcolor="#CCCCCC"><strong>รายละเอียดสินค้า</strong></td>
     </tr>
   </thead>
   <tbody>
   <?php
   $i=0;
-  $getneed_checkin_today=$getdata->my_sql_select(NULL,"product","dealer_code='".@$getmember_detail->dealer_code."' ");
+  $getneed_checkin_today=$getdata->my_sql_selectJoin(" p.*, r.*, w.* ,w.diameter as diameterWheel,r.diameter as diameterRubber,p.ProductID as ProductID,r.diameter as rubdiameter ,w.diameter as whediameter
+  ,case
+  when p.TypeID = '2'
+  then (select b.Description from brandRubble b where r.brand = b.id)
+  when p.TypeID = '1'
+  then (select b.Description from BrandWhee b where b.id = w.brand)
+  end BrandName"
+  ,"product_N "
+  ,"productDetailWheel w on p.ProductID = w.ProductID
+  left join productdetailrubber r on p.ProductID = r.ProductID "
+  ,"Where p.dealer_code='".@$getmember_detail->dealer_code."' ");
   while($showneed_checkin_today = mysql_fetch_object($getneed_checkin_today)){
     $i++;
-    $getbrand =$getdata->my_sql_query("BrandName","brand","BrandID='".$showneed_checkin_today->BrandID."'");
-    $getType =$getdata->my_sql_query("TypeName","type","TypeID='".$showneed_checkin_today->TypeID."'");
+    if($showneed_checkin_today->TypeID == '1'){
+      $gettype = "ล้อแม๊ก ".$showneed_checkin_today->BrandName." ขนาด:".$showneed_checkin_today->diameterWheel." ขอบ:".$showneed_checkin_today->whediameter." รู:".$showneed_checkin_today->holeSize." ประเภท:".$showneed_checkin_today->typeFormat;
+    }else if($showneed_checkin_today->TypeID == '2'){
+      $gettype = "ยาง ".$showneed_checkin_today->BrandName." ขนาด:".$showneed_checkin_today->diameterRubber." ซี่รี่:".$showneed_checkin_today->series." ความกว้าง:".$showneed_checkin_today->width;
+    }else{
+      $gettype = "";
+    }
   ?>
   <tr>
     <td align="center"><?php echo @$i;?></td>
-    <td align="center"><strong><?php echo @$showneed_checkin_today->ProductID;?></strong></td>
-    <td align="center"><?php echo @$showneed_checkin_today->ProductName;?></td>
-    <td align="center"><?php echo @$getbrand->BrandName;?></td>
-    <td align="center"><?php echo @$getType->TypeName;?></td>
+    <td align=""><strong><?php echo @$gettype;?></strong></td>
     </tr>
-  
+
   <?php
   }
   ?>
